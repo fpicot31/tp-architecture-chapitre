d0b3e4c20d0b2ca64552d69c5015cb725137c72f
fpicot31 <80047098+fpicot31@users.noreply.github.com>
2022-04-21 20:19:58 +0200

    Add files via upload

---
+++ init-mongo.js
@@ -0,0 +1,15 @@
+
+db.createCollection('chapters');
+
+db.chapters.insertMany([
+    {
+        title: 'git', 
+        index : "1", 
+        nbrOfLessons: 20
+    }, 
+    {
+        title: 'devops', 
+        index : "2", 
+        nbrOfLessons: 30
+    }
+]);
