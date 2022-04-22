55174649ed8ab51ed9487e8a8d2682ad5c40e74a
fpicot31 <80047098+fpicot31@users.noreply.github.com>
2022-04-22 11:38:12 +0200

    connect to mongo db cloud

--- database/index.js
+++ database/index.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose
   .connect(
-    'mongodb://francois:francois@localhost:27017/demo?retryWrites=true',
+    /* Pour une base sur le cloud cloud.mongodb.com avec le compte gmail */
+    'mongodb+srv://francois:francois@cluster0.jzi6n.mongodb.net/demo?retryWrites=true&w=majority',
+    /*'mongodb://francois:francois@localhost:27017/demo?retryWrites=true',*/
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
